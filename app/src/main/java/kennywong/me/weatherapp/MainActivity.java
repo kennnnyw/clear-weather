6f1277944464411c521a00cc867f521989dee2d7
Kenny Wong <kennywongbl@gmail.com>
2018-06-02 14:11:28 +0100

    Added themes

--- app/src/main/java/kennywong/me/weatherapp/MainActivity.java
+++ app/src/main/java/kennywong/me/weatherapp/MainActivity.java
@@ -2,12 +2,16 @@ package kennywong.me.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.DayTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
