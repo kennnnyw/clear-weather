f411a9e369a6fd960d52c88bf290ca4e8f2cb86f
Kenny Wong <kennywongbl@gmail.com>
2018-06-02 15:02:25 +0100

    Began implementating main screen UI

--- app/src/main/java/kennywong/me/weatherapp/MainActivity.java
+++ app/src/main/java/kennywong/me/weatherapp/MainActivity.java
@@ -2,16 +2,44 @@ package kennywong.me.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.DayTheme);
+        updateTheme();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    /**
+     * Change the colours of the app depending on the time of day.
+     * 4:00 - 9:59 = Morning
+     * 10:00 - 19:59 = Day
+     * 20:00 - 3:59 = Night
+     */
+    public void updateTheme(){
+        Calendar now = Calendar.getInstance();
+        int hour = now.get(Calendar.HOUR_OF_DAY);
+
+        if (hour < 10 && hour >= 4) {
+            // morning
+            setTheme(R.style.MorningTheme);
+        } else if (hour >= 10  && hour < 20) {
+            // day
+            setTheme(R.style.DayTheme);
+        } else if (hour >= 20 || hour < 4) {
+            // night
+            setTheme(R.style.NightTheme);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateTheme();
+        setContentView(R.layout.activity_main);
     }
 }
